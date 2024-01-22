d8617f7f79227c5dca0e3582013b1aee942a6c5f
patrr2 <unknown>
2024-01-22 10:34:43 +0200

    switch top prompt answer to the model answer

--- webfront/src/components/SlidingDifficultyQuestionCard.tsx
+++ webfront/src/components/SlidingDifficultyQuestionCard.tsx
@@ -18,14 +18,14 @@ export default (props : { task:  SlidingDifficultyTaskResultsObject }) => {
     const sortedResults = sortByDifficultyAndPassed(Object.entries(task.results))
     const topDifficulty = sortedResults[0][1].highestDifficultyPassed
     const topDifficultyPromptExample = sortedResults[0][1].evaluations.filter(x => x.difficulty === topDifficulty)[0]
-    const TopDifficultyPromptAnswerJsx = topDifficultyPromptExample.responseText.length > 30 ? <textarea id={answerAreaId} className="w-full h-6">{topDifficultyPromptExample.responseText}</textarea> : topDifficultyPromptExample.responseText
+    const TopDifficultyPromptAnswerJsx = topDifficultyPromptExample.humanReadableSolution
     
     return (
         <div className="w-full rounded-md bg-gray-950 p-4">
             <h4 className="text-lg">{task.taskName}</h4>
             <div className="rounded-md bg-gray-900 p-2">
                 <b>Top Prompt: </b> {topDifficultyPromptExample.promptText} <br />
-                <b>Answer: </b> {TopDifficultyPromptAnswerJsx} <br />
+                <b>Correct Answer: </b> {TopDifficultyPromptAnswerJsx} <br />
             </div>
             <div className="my-2 flex gap-2 ml-1">
                 <b className="leading-8">Scores:</b>
